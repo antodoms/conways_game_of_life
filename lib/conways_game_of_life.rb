9b858fd7543bc12288b6ae0c56329fa646afa732
Anto Dominic <antodoms@gmail.com>
2017-10-07 21:14:54 +1100

    Added more methods to board.

--- lib/conways_game_of_life.rb
+++ lib/conways_game_of_life.rb
@@ -1,8 +1,9 @@
-require "conways_game_of_life/version"
-require "conways_game_of_life/game"
-require "conways_game_of_life/board"
-require "conways_game_of_life/cell"
+require 'conways_game_of_life/version'
+require 'conways_game_of_life/game'
+require 'conways_game_of_life/board'
+require 'conways_game_of_life/cell'
 
+# conways game of life
 module ConwaysGameOfLife
-  
+  # conways game of life
 end
--- spec/conways_game_of_life_spec.rb
+++ spec/conways_game_of_life_spec.rb
@@ -1,11 +1,11 @@
-require "spec_helper"
+require 'spec_helper'
 
 RSpec.describe ConwaysGameOfLife do
-  it "has a version number" do
+  it 'has a version number' do
     expect(ConwaysGameOfLife::VERSION).not_to be nil
   end
 
-  it "does something useful" do
+  it 'does something useful' do
     expect(false).to eq(false)
   end
 end
